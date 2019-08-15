e8db607c46468d5f4457a9865edbb7cf6d100dad
twihell <sevlost@gmail.com>
2019-08-15 21:32:14 +0300

    add new functions

--- lection1/index.js
+++ lection1/index.js
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+
+function fn (number) {
+    if (number > 50 && number <= 20) {
+        console.log(true);
+    } 
+        console.log(false);
+}
+
+fn (12);
+
+fn2 (a,b) {
+a = a+b;
+b = a-b;
+b = a-b;
+console.log(a,b);
+}
+
+fn2(2,3);
