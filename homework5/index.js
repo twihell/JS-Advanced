bc5f5c17111a3005501c763ab4d8dcf1ea09d3df
twihell <sevlost@gmail.com>
2019-09-05 13:17:46 +0300

    add homework5

---
+++ homework5/index.js
@@ -0,0 +1,45 @@
+const inp = document.querySelector('input');
+const addTaskButton = document.querySelector('button');
+const list = document.querySelector('ul');
+const checkBox = document.getElementsByClassName('check')[0];
+const priorityBtn = document.getElementsByClassName('priorityBtn')[0];
+
+
+priorityBtn.addEventListener('click', () => {
+    if(!checkBox.checked) {
+        checkBox.checked = true;
+    } else {
+        checkBox.checked = false;
+    }
+
+});
+
+
+function getInputValue(inputElement) {
+    return inputElement.value;
+}
+
+
+function createElementWithText(elementName, text) {
+    let el = document.createElement(elementName);
+    el.textContent = text;
+    return el;
+}
+
+function appendChildTodo(parent, child) {
+    parent.appendChild(child);
+}
+
+addTaskButton.addEventListener('click', () => {
+    if (getInputValue(inp)) {
+        let child = createElementWithText('li', getInputValue(inp));
+        if (checkBox.checked) {
+            child.style.color = "red";
+            list.insertBefore(child, list.firstChild);
+        } else {
+            appendChildTodo(list, child);
+        }
+    }
+
+});
+
