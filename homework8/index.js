aed4d64b4dba6a7781aae971669d836196514721
twihell <sevlost@gmail.com>
2019-09-17 00:10:27 +0300

    add edited version

--- homework8/index.js
+++ homework8/index.js
@@ -12,25 +12,22 @@ const lastFolFollowers = document.querySelector('#fifthFolFollowers');
 
 
 btnGetUser.addEventListener('click', () => {
-    if(searchInput.value) {
+    if (searchInput.value) {
         getUser(searchInput.value);
         getUserFollowers(searchInput.value);
-        
     }
-    
-
 });
 
 const getUser = (user) => {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', `${gitHubApiUrl}/${user}`);
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState === 4 && xhr.status === 200) {
-          parseData(JSON.parse(xhr.responseText));
-      }
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            parseData(JSON.parse(xhr.responseText));
+        }
     };
     xhr.send();
-  };
+};
 
 const parseData = (object) => {
     avatar.src = object["avatar_url"];
@@ -38,84 +35,68 @@ const parseData = (object) => {
     user.textContent = object["login"];
     followers.href = object["followers_url"];
     followers.textContent = "Followers!";
-      
-
 };
 
 const getUserFollowers = (user) => {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', `${gitHubApiUrl}/${user}/followers`);
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState === 4 && xhr.status === 200) {
-        let followersArray = parseFollowers(JSON.parse(xhr.responseText));
-        getSecondaryFollowers(followersArray[0]);
-        
-        // getSecondaryFollowers(followersArray[4]);
-      }
-
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let followersArray = parseFollowers(JSON.parse(xhr.responseText));
+            getTrueSecondaryFollowers(followersArray[0], firstFolFollowers);
+            getTrueSecondaryFollowers(followersArray[4], lastFolFollowers);
+        }
     };
     xhr.send();
+};
 
-  };
-
-  const parseFollowers = (arrayOfFollowers) => {
+const parseFollowers = (arrayOfFollowers) => {
     let selectedFollowers = arrayOfFollowers.slice(0, 5);
-      for(i = 0; i < selectedFollowers.length; i++) {
-    
-          let followerProfile = selectedFollowers[i];
-          let img = document.createElement('img');
-          let caption = document.createElement('figcaption');
-          folAvatars.appendChild(img);
-          folAvatars.appendChild(caption);
-          img.src = followerProfile["avatar_url"];
-          img.id = "folPic";
-          caption.textContent = followerProfile["login"];
+    for (i = 0; i < selectedFollowers.length; i++) {
 
-      }
-      return selectedFollowers;
-  };
+        let followerProfile = selectedFollowers[i];
+        let img = document.createElement('img');
+        let caption = document.createElement('figcaption');
+        folAvatars.appendChild(img);
+        folAvatars.appendChild(caption);
+        img.src = followerProfile["avatar_url"];
+        img.id = "folPic";
+        caption.textContent = followerProfile["login"];
+
+    }
+    return selectedFollowers;
+};
 
 
-  const getSecondaryFollowers = (user) => {
+const getTrueSecondaryFollowers = (user, parentNode) => {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', user.followers_url);
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState === 4 && xhr.status === 200) {
-        parseFirstUserFollowers(JSON.parse(xhr.responseText));
-      }
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            parseNestedFollowers(JSON.parse(xhr.responseText), parentNode);
+        }
     };
     xhr.send();
-  };
+};
 
-  const parseFirstUserFollowers = (arrayOfFollowers) => {
+const userCard = (profile) => {
+    let wrapper = document.createElement('figure');
+    let img = document.createElement('img');
+    let caption = document.createElement('figcaption');
+    img.src = profile["avatar_url"];
+    img.id = "secondaryFolPic";
+    caption.textContent = profile["login"];
+    wrapper.appendChild(img);
+    wrapper.appendChild(caption);
+    return wrapper;
+}
+
+const parseNestedFollowers = (arrayOfFollowers, parentNode) => {
     let selectedFollowers = arrayOfFollowers.slice(0, 5);
-    
-    for(i = 0; i < selectedFollowers.length; i++) {
-    
+    for (i = 0; i < selectedFollowers.length; i++) {
+
         let followerProfile = selectedFollowers[i];
-        let img = document.createElement('img');
-        let caption = document.createElement('figcaption');
-        firstFolFollowers.appendChild(img);
-        firstFolFollowers.appendChild(caption);
-        img.src = followerProfile["avatar_url"];
-        img.id = "secondaryFolPic";
-        caption.textContent = followerProfile["login"];
+        parentNode.appendChild(userCard(followerProfile));
 
     }
-  };
-
-  const parseLastUserFollowers = (arrayOfFollowers) => {
-    let selectedFollowers = arrayOfFollowers.slice(0, 5);
-    for(i = 0; i < selectedFollowers.length; i++) {
-    
-        let followerProfile = selectedFollowers[i];
-        let img = document.createElement('img');
-        let caption = document.createElement('figcaption');
-        lastFolFollowers.appendChild(img);
-        lastFolFollowers.appendChild(caption);
-        img.src = followerProfile["avatar_url"];
-        img.id = "secondaryFolPic";
-        caption.textContent = followerProfile["login"];
-
-    }
-  };
+};
